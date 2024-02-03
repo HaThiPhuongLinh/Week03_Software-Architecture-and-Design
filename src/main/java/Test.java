8083f5ffb7eed0ac7915067e5fa62e45800250c0
DESKTOP-NQDA7TS\ThanhTu <tuthanh375@gmail.com>
2024-02-03 14:35:32 +0700

    fix to only accept package include edu.iuh

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,24 +1,34 @@
-import jdepend.xmlui.JDepend;
+import jdepend.framework.JDepend;
+import jdepend.framework.JavaPackage;
+import jdepend.framework.PackageFilter;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        String proDir = "D:\\JDepend";
-        String XMLFilePath = "jdepend.xml";
+        JDepend jDepend = new JDepend();
+        jDepend.addDirectory("D:\\TestJDepend");
 
-        File xmlFile = new File(XMLFilePath);
-        if(!xmlFile.exists()){
-            xmlFile.createNewFile();
+        Collection<JavaPackage> cols = jDepend.analyze();
+        cols.forEach(pkg -> {
+            System.out.printf("Package %s, Ca = %d, Ce = %d; \n", pkg.getName(), pkg.getAfferents().size(), pkg.getEfferents().size());
+        });
+
+        try (PrintWriter writer = new PrintWriter("results.xml")) {
+            jdepend.xmlui.JDepend xml = new jdepend.xmlui.JDepend(writer);
+            xml.addDirectory("D:\\TestJDepend");
+
+            PackageFilter filter = PackageFilter.all();
+            //filter.including("edu.iuh");
+            filter.accept("edu.iuh");
+            filter.excluding("com", "org", "java", "javax");
+
+            xml.setFilter(filter);
+            xml.analyze();
         }
 
-        try (PrintStream out = new PrintStream(new FileOutputStream(xmlFile))) {
-            System.setOut(out);
-
-            JDepend depend = new JDepend();
-            depend.addDirectory(proDir);
-            depend.analyze();
-        }
     }
 
 }
